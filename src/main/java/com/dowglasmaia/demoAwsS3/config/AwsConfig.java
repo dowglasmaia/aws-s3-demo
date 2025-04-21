4ec5cf3f44be93d02016a97acae0b9d236e4240c
Dowglas Maia <dowglasmaia@gmail.com>
2025-04-21 15:24:26 -0300

    Migrating AWS S3 v1 to v2

--- src/main/java/com/dowglasmaia/demoAwsS3/config/AwsConfig.java
+++ src/main/java/com/dowglasmaia/demoAwsS3/config/AwsConfig.java
@@ -1,12 +1,13 @@
 package com.dowglasmaia.demoAwsS3.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AwsConfig {
@@ -22,13 +23,14 @@ public class AwsConfig {
 
 
     @Bean
-    public AmazonS3 amazonS3(){
-        final BasicAWSCredentials credentials =  new BasicAWSCredentials(accessKey,secretKey);
-        return AmazonS3ClientBuilder
-                .standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .build();
+    public S3Client s3Client(){
+       return S3Client.builder()
+               .region(Region.of(region))
+               .credentialsProvider(
+                       StaticCredentialsProvider.create(
+                               AwsBasicCredentials.create(accessKey,secretKey)
+                       )
+               ).build();
     }
 
 }
--- src/main/java/com/dowglasmaia/demoAwsS3/service/S3Service.java
+++ src/main/java/com/dowglasmaia/demoAwsS3/service/S3Service.java
@@ -1,11 +1,12 @@
 package com.dowglasmaia.demoAwsS3.service;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Service
 public class S3Service {
@@ -13,21 +14,22 @@ public class S3Service {
     @Value("${aws.bucket-name}")
     private String bucketeName;
 
-    private AmazonS3 amazonS3;
+    private S3Client s3Client;
 
     @Autowired
-    public S3Service(AmazonS3 amazonS3) {
-        this.amazonS3 = amazonS3;
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
     }
 
     public void uploadPdf(String fileName, MultipartFile file){
        try {
-           ObjectMetadata  metadata = new ObjectMetadata();
-           metadata.setContentType("application/pdf");
-           metadata.setContentLength(file.getSize());
+           PutObjectRequest request = PutObjectRequest.builder()
+                           .bucket(bucketeName)
+                           .key(fileName)
+                           .contentType("application/pdf")
+                           .build();
 
-           amazonS3.putObject(bucketeName,fileName,file.getInputStream(),metadata);
-
+           s3Client.putObject(request, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
        } catch (Exception e) {
            throw  new RuntimeException("Failed to upload PDF to s3", e);
        }
